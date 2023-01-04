2658e8f22ce0f6d146ea8a927f596ff15f27c626
khoi2 <khoi2@gmail.com>
2023-01-04 14:50:31 +0700

    Done

--- index.js
+++ index.js
@@ -26,21 +26,26 @@ async function Check(url, username, password) {
   try {
     const page = await browser.newPage()
     await page.goto(url)
-    await page.waitForSelector('#username')
+    // await page.waitForSelector('form')
+    await page.type('input[type="text"]', username, { delay: 20 })
+    await page.type('input[type="password"]', password, { delay: 20 })
 
-    await page.type('#username', username, { delay: 100 })
-    await page.type('#password', password, { delay: 100 })
     await page.keyboard.press('Enter')
+    try {
+      await page.click('button')
+    } catch (error) {}
+
     await page.waitForTimeout(2000)
     if (
-      !page.url().includes('auth') ||
-      !page.url().includes('Login') ||
+      !page.url().includes('auth') &&
+      !page.url().includes('Login') &&
       !page.url().includes('login')
     ) {
       console.log(url, username, password)
     }
     await browser.close()
   } catch (e) {
+    // console.log(e.message)
     await browser.close()
   }
 }
